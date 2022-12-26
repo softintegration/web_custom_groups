0250197d5b00f7ad9a022b5f04ee6da57094417d
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-26 08:21:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Web custom groups
+------------------
+This module add som custom css groups useful for backend design in some cases.  
+
+
+
+
---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Web custom groups',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Hidden',
+    'demo': [],
+    'depends': [],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'web_custom_groups/static/src/css/web_custom_groups.css',
+        ],
+    },
+    'license': 'LGPL-3',
+}
